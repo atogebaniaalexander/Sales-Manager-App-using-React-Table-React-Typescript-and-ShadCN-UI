4ef16c7fba55452b688654ccb2311d66b8d04ea6
Atogebania Alexander <aatogebania@gmail.com>
2025-01-31 00:58:27 +0000

    stopped at card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./Components/theme-provider"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100","200","300","400","500","600","700","800","900"],
+  
+  
 });
 
 export const metadata: Metadata = {
@@ -25,9 +24,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
+        suppressHydrationWarning
       >
-        {children}
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            
+          >
+            {children},
+            
+          </ThemeProvider>
+          
+        </body>  
       </body>
     </html>
   );
